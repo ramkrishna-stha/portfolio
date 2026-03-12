ee955a76c4e5993af622191a912ce2b41df87114
ramkrishna-stha <sthram132@gmail.com>
2026-03-12 18:50:03 +0545

    first commit

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,15 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
